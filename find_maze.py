83ed2d8c0f4956e00c8b37fee145995cfb4aed5c
seven320 <yosyuaomenw@yahoo.co.jp>
2018-05-17 14:10:22 +0900

    find_maze ver1.2

--- find_maze.py
+++ find_maze.py
@@ -9,16 +9,14 @@ import numpy as np
 # x=int(input())
 # print("迷路のサイズを入力してください　Y:",end="")
 # y=int(input())
-X=10
-Y=10
+X=5
+Y=5
 # plt.plot(x_1,y_1)
 for i in range(X+1):
-    plt.vlines(i,0,Y,alpha=0.5)#alpha:透過率
+    plt.vlines(i,0,Y,alpha=0.8)#alpha:透過率
 for j in range(Y+1):
-    plt.hlines(j,0,X,alpha=0.5)
+    plt.hlines(j,0,X,alpha=0.8)
 plt.axis([0,X,0,Y])#plt.axis(xmin, xmax, ymin, ymax)
-
-
 
 def input_x_y(str,X,Y,ban_list):
     while(True):
@@ -43,19 +41,20 @@ def input_x_y(str,X,Y,ban_list):
             break
     return list
 
-def print_setting(status,x_y):
-    pass
 
 # setting
 ban_list=[[0,1]]
 start=input_x_y("スタート",X,Y,ban_list)
-# goal=input_x_y("ゴール",X,Y,ban_list)
-# obj_num=3
-# obj=[]
-# for i in range(obj_num):
-#     obj.append(input_x_y("障害物"+str(i),X,Y,ban_list))
+goal=input_x_y("ゴール",X,Y,ban_list)
+obj_num=3
+obj=[]
+for i in range(obj_num):
+    obj.append(input_x_y("障害物"+str(i),X,Y,ban_list))
 
-
-plt.plot(start[0],start[1],"ro")
-plt.plot(goal[0],goal[1],"g^")
+mk=15#markersize
+plt.plot(start[0],start[1],label="start",marker="o",markerfacecolor="g",markersize=mk,mec="g")
+plt.plot(goal[0],goal[1],label="goal",marker="o",markerfacecolor="b",markersize=mk,mec="b")
+plt.legend()#label表示
+for i in range(obj_num):
+    plt.plot(obj[i][0],obj[i][1],marker="x",mec="r",markersize=20,lw=3)
 plt.show()
